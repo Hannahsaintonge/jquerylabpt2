3e45507d8599a275e160ef76b18f30a80ee28e68
hannahsaintonge <hannahsaintonge@gmail.com>
2018-05-09 07:37:01 -0400

    first commit for part 2

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+$(document).ready(() => {
+
+  let btn = $("#table_button");
+ 
+  btn.click(() => {
+    $(".reservation_form").toggle();
+    //btn = $(event.target);
+    //$(".reservation_form").fadeIn(1000);
+
+});
+
+
+let exit = $(".material-icons")
+
+exit.click(() => {
+    $(".reservation_form").toggle();
+
+});
+
+let exitSave = $(".save_button");
+
+exitSave.click(() => {
+    $(".reservation_form").toggle();
+
+});
+
+
+//let btn = $("#table_button");
+//$("btn").on("click", function(e){
+    //table.addClass("reserved");
+    //table.removeClass("available");
+    //$(".reservation_form").hide();
+
+
+//$("btn").on("click", (event) => {
+    //btn = $(event.target);
+    //$(".reservation_form").fadeIn(1000);
+  //});
+
+  //$("btn").click(function() {
+   
+//});
+
+let 
+
+
+
+});
+
